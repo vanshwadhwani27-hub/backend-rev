76bd6b7bd31762bba7b7a22f8744187483c1d112
vansh wadhwani <vanshwadhwani27@gmail.com>
2026-08-14 16:00:18 +0530

    connecting to database and creating server

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Server connected to MongoDB successfully");
+    } catch (err) {
+        console.log("There was an error while connecting to the database");
+        console.error(err);
+    }
+};
+
+module.exports = connectToDB;
